6973af63d804f7fa0297818b6c208255272a638a
Gabriel Sandu <allmediacreation@gmail.com>
2019-04-05 15:05:32 +0300

    Updates

---
+++ precache-manifest.56c6d50f29d2d1ff8a89dcd83f23298e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Movies-List/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "ebf103b2aa9db5552b42",
+    "url": "/React-Movies-List/static/css/main.1d41d620.chunk.css"
+  },
+  {
+    "revision": "1e534bcc6fa005ad2650",
+    "url": "/React-Movies-List/static/js/runtime~main.d4e18010.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Movies-List/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e363ddeb7e1f8f142e8f",
+    "url": "/React-Movies-List/static/js/2.1c835c35.chunk.js"
+  },
+  {
+    "revision": "ebf103b2aa9db5552b42",
+    "url": "/React-Movies-List/static/js/main.d3fc1e31.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Movies-List/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Movies-List/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Movies-List/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "e363ddeb7e1f8f142e8f",
+    "url": "/React-Movies-List/static/css/2.f9999ffb.chunk.css"
+  },
+  {
+    "revision": "dac5b4398d46c5e983e18600f2df44bd",
+    "url": "/React-Movies-List/index.html"
+  }
+];
